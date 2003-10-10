015dde92f75ed68fe463a5b49fe25b22bdd39011
scott <scott@adligo.com>
2003-10-10 00:37:25 +0000

    *** empty log message ***

--- src/org/adligo/tests/xml/parsers/template/Suite.java
+++ src/org/adligo/tests/xml/parsers/template/Suite.java
@@ -9,33 +9,10 @@ package org.adligo.tests.xml.parsers.template;
  * @version 1.0
  */
 import junit.framework.*;
-import org.apache.log4j.BasicConfigurator;
 import java.lang.reflect.*;
 
-public class Suite {
-
-  public Suite() {
-  }
-
-  public static String getPackage() {
-    return "org.adligo.tests.xml.parsers.template.";
-  }
-
+public class Suite extends org.adligo.tests.Suite {
   public static Test suite() {
-    //BasicConfigurator.configure();
-    TestSuite suite= new TestSuite();
-    try {
-      for (int i = 0; i < 6; i++) {
-        Class c = Class.forName(getPackage() + "Test" + i);
-        Class [] args = new Class[] {String.class};
-        Constructor ct = c.getConstructor(args);
-        String [] sa = new String [] {"test" + i};
-        Object o = ct.newInstance(sa);
-        suite.addTest((TestCase) o);
-      }
-    } catch (Exception x) {x.printStackTrace(); }
-
-    //suite.addTest(new Test0("test0"));
-    return suite;
+    return suite("org.adligo.tests.xml.parsers.template.");
   }
 }
