14fd67b49d4c98a01db93a5e694c02f0315900bb
scott <scott@adligo.com>
2010-12-31 05:00:04 +0000

    added tests

---
+++ src/org/adligo/tests/xml/parsers/template/jdbc/MockConnection.java
@@ -0,0 +1,295 @@
+package org.adligo.tests.xml.parsers.template.jdbc;
+
+import java.sql.Array;
+import java.sql.Blob;
+import java.sql.CallableStatement;
+import java.sql.Clob;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.NClob;
+import java.sql.PreparedStatement;
+import java.sql.SQLClientInfoException;
+import java.sql.SQLException;
+import java.sql.SQLWarning;
+import java.sql.SQLXML;
+import java.sql.Savepoint;
+import java.sql.Statement;
+import java.sql.Struct;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+public class MockConnection implements Connection {
+	private Connection delegate;
+	private List<Statement> stmtsReturned = new ArrayList<Statement>();
+	private List<PreparedStatement> prepStmtsReturned = new ArrayList<PreparedStatement>();
+	private boolean calledCommit = false;
+	
+	public MockConnection(Connection p) {
+		delegate = p;
+	}
+	
+	public Connection getDelegate() {
+		return delegate;
+	}
+
+	public void setDelegate(Connection delegate) {
+		this.delegate = delegate;
+	}
+
+	
+	/**
+	 * resets the mock state to a new object
+	 */
+	public void reset() {
+		stmtsReturned = new ArrayList<Statement>();
+		prepStmtsReturned = new ArrayList<PreparedStatement>();
+		calledCommit = false;
+	}
+	
+	public <T> T unwrap(Class<T> iface) throws SQLException {
+		return delegate.unwrap(iface);
+	}
+
+	public boolean isWrapperFor(Class<?> iface) throws SQLException {
+		return delegate.isWrapperFor(iface);
+	}
+
+	public Statement createStatement() throws SQLException {
+		Statement toRet =  delegate.createStatement();
+		stmtsReturned.add(toRet);
+		return toRet;
+	}
+	
+	public List<Statement> getCreatedStatements() {
+		return stmtsReturned;
+	}
+	
+	public void clearCreatedStatements() {
+		stmtsReturned.clear();
+	}
+
+	public PreparedStatement prepareStatement(String sql) throws SQLException {
+		PreparedStatement toRet =  delegate.prepareStatement(sql);
+		prepStmtsReturned.add(toRet);
+		return toRet;
+	}
+
+	public List<PreparedStatement> getCreatedPreparedStatement() {
+		return prepStmtsReturned;
+	}
+	
+	public void clearCreatedPreparedStatement() {
+		prepStmtsReturned.clear();
+	}
+	
+	public CallableStatement prepareCall(String sql) throws SQLException {
+		return delegate.prepareCall(sql);
+	}
+
+	public String nativeSQL(String sql) throws SQLException {
+		return delegate.nativeSQL(sql);
+	}
+
+	public void setAutoCommit(boolean autoCommit) throws SQLException {
+		delegate.setAutoCommit(autoCommit);
+	}
+
+	public boolean getAutoCommit() throws SQLException {
+		return delegate.getAutoCommit();
+	}
+
+	public void commit() throws SQLException {
+		calledCommit = true;
+		delegate.commit();
+	}
+	
+	public boolean calledCommit() {
+		return calledCommit;
+	}
+	
+	public void setCalledCommit() {
+		calledCommit = false;
+	}
+
+	public void rollback() throws SQLException {
+		delegate.rollback();
+	}
+
+	public void close() throws SQLException {
+		delegate.close();
+	}
+
+	public boolean isClosed() throws SQLException {
+		return delegate.isClosed();
+	}
+
+	public DatabaseMetaData getMetaData() throws SQLException {
+		return delegate.getMetaData();
+	}
+
+	public void setReadOnly(boolean readOnly) throws SQLException {
+		delegate.setReadOnly(readOnly);
+	}
+
+	public boolean isReadOnly() throws SQLException {
+		return delegate.isReadOnly();
+	}
+
+	public void setCatalog(String catalog) throws SQLException {
+		delegate.setCatalog(catalog);
+	}
+
+	public String getCatalog() throws SQLException {
+		return delegate.getCatalog();
+	}
+
+	public void setTransactionIsolation(int level) throws SQLException {
+		delegate.setTransactionIsolation(level);
+	}
+
+	public int getTransactionIsolation() throws SQLException {
+		return delegate.getTransactionIsolation();
+	}
+
+	public SQLWarning getWarnings() throws SQLException {
+		return delegate.getWarnings();
+	}
+
+	public void clearWarnings() throws SQLException {
+		delegate.clearWarnings();
+	}
+
+	public Statement createStatement(int resultSetType, int resultSetConcurrency)
+			throws SQLException {
+		return delegate.createStatement(resultSetType, resultSetConcurrency);
+	}
+
+	public PreparedStatement prepareStatement(String sql, int resultSetType,
+			int resultSetConcurrency) throws SQLException {
+		return delegate.prepareStatement(sql, resultSetType,
+				resultSetConcurrency);
+	}
+
+	public CallableStatement prepareCall(String sql, int resultSetType,
+			int resultSetConcurrency) throws SQLException {
+		return delegate.prepareCall(sql, resultSetType, resultSetConcurrency);
+	}
+
+	public Map<String, Class<?>> getTypeMap() throws SQLException {
+		return delegate.getTypeMap();
+	}
+
+	public void setTypeMap(Map<String, Class<?>> map) throws SQLException {
+		delegate.setTypeMap(map);
+	}
+
+	public void setHoldability(int holdability) throws SQLException {
+		delegate.setHoldability(holdability);
+	}
+
+	public int getHoldability() throws SQLException {
+		return delegate.getHoldability();
+	}
+
+	public Savepoint setSavepoint() throws SQLException {
+		return delegate.setSavepoint();
+	}
+
+	public Savepoint setSavepoint(String name) throws SQLException {
+		return delegate.setSavepoint(name);
+	}
+
+	public void rollback(Savepoint savepoint) throws SQLException {
+		delegate.rollback(savepoint);
+	}
+
+	public void releaseSavepoint(Savepoint savepoint) throws SQLException {
+		delegate.releaseSavepoint(savepoint);
+	}
+
+	public Statement createStatement(int resultSetType,
+			int resultSetConcurrency, int resultSetHoldability)
+			throws SQLException {
+		return delegate.createStatement(resultSetType, resultSetConcurrency,
+				resultSetHoldability);
+	}
+
+	public PreparedStatement prepareStatement(String sql, int resultSetType,
+			int resultSetConcurrency, int resultSetHoldability)
+			throws SQLException {
+		return delegate.prepareStatement(sql, resultSetType,
+				resultSetConcurrency, resultSetHoldability);
+	}
+
+	public CallableStatement prepareCall(String sql, int resultSetType,
+			int resultSetConcurrency, int resultSetHoldability)
+			throws SQLException {
+		return delegate.prepareCall(sql, resultSetType, resultSetConcurrency,
+				resultSetHoldability);
+	}
+
+	public PreparedStatement prepareStatement(String sql, int autoGeneratedKeys)
+			throws SQLException {
+		return delegate.prepareStatement(sql, autoGeneratedKeys);
+	}
+
+	public PreparedStatement prepareStatement(String sql, int[] columnIndexes)
+			throws SQLException {
+		return delegate.prepareStatement(sql, columnIndexes);
+	}
+
+	public PreparedStatement prepareStatement(String sql, String[] columnNames)
+			throws SQLException {
+		return delegate.prepareStatement(sql, columnNames);
+	}
+
+	public Clob createClob() throws SQLException {
+		return delegate.createClob();
+	}
+
+	public Blob createBlob() throws SQLException {
+		return delegate.createBlob();
+	}
+
+	public NClob createNClob() throws SQLException {
+		return delegate.createNClob();
+	}
+
+	public SQLXML createSQLXML() throws SQLException {
+		return delegate.createSQLXML();
+	}
+
+	public boolean isValid(int timeout) throws SQLException {
+		return delegate.isValid(timeout);
+	}
+
+	public void setClientInfo(String name, String value)
+			throws SQLClientInfoException {
+		delegate.setClientInfo(name, value);
+	}
+
+	public void setClientInfo(Properties properties)
+			throws SQLClientInfoException {
+		delegate.setClientInfo(properties);
+	}
+
+	public String getClientInfo(String name) throws SQLException {
+		return delegate.getClientInfo(name);
+	}
+
+	public Properties getClientInfo() throws SQLException {
+		return delegate.getClientInfo();
+	}
+
+	public Array createArrayOf(String typeName, Object[] elements)
+			throws SQLException {
+		return delegate.createArrayOf(typeName, elements);
+	}
+
+	public Struct createStruct(String typeName, Object[] attributes)
+			throws SQLException {
+		return delegate.createStruct(typeName, attributes);
+	}
+}
--- src/org/adligo/tests/xml/parsers/template/jdbc/TestJdbcQueries.java
+++ src/org/adligo/tests/xml/parsers/template/jdbc/TestJdbcQueries.java
@@ -1,8 +1,10 @@
 package org.adligo.tests.xml.parsers.template.jdbc;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
+import java.util.List;
 
 import org.adligo.i.log.client.Log;
 import org.adligo.i.log.client.LogFactory;
@@ -15,6 +17,7 @@ import org.adligo.xml.parsers.template.TemplateParserEngine;
 import org.adligo.xml.parsers.template.Templates;
 import org.adligo.xml.parsers.template.jdbc.BaseSqlOperators;
 import org.adligo.xml.parsers.template.jdbc.JdbcEngineInput;
+import org.adligo.xml.parsers.template.jdbc.JdbcQueryResult;
 import org.adligo.xml.parsers.template.jdbc.JdbcTemplateParserEngine;
 import org.adligo.xml.parsers.template.jdbc.PrettyParamDecorator;
 
@@ -22,25 +25,33 @@ import org.adligo.xml.parsers.template.jdbc.PrettyParamDecorator;
 public class TestJdbcQueries extends ATest {
 	private static final Log log = LogFactory.getLog(TestJdbcQueries.class);
 	Templates templates = new Templates();
+	Templates executeUpdateTemplates = new Templates();
+	
 	SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
 	
 	
 	public void setUp() throws SQLException {
 		MockDatabase.createTestDb();
-		templates.parseResource("/org/adligo/tests/xml/parsers/template/jdbc/Persons2_0_SQL.xml");
+		templates.parseResource(
+				"/org/adligo/tests/xml/parsers/template/jdbc/Persons2_0_SQL.xml");
+		executeUpdateTemplates.parseResource(
+				"/org/adligo/tests/xml/parsers/template/jdbc/ExecuteUpdate.xml");
 	}
 	
 	
-	private ResultSet getResultSet(Params params, Template template)
+	private JdbcQueryResult getResultSet(Params params, Template template)
 			throws SQLException {
 		JdbcEngineInput jdbcValues = new JdbcEngineInput();
 		jdbcValues.setAllowedOperators(BaseSqlOperators.OPERATORS);
 		jdbcValues.setParams(params);
 		
+		MockConnection mockConnection = new MockConnection(MockDatabase.getMemConnection());
 		jdbcValues.setTemplate(template);
-		jdbcValues.setConnection(MockDatabase.getMemConnection());
-		ResultSet rs = JdbcTemplateParserEngine.query(jdbcValues);
-		return rs;
+		jdbcValues.setConnection(mockConnection);
+		JdbcQueryResult result = JdbcTemplateParserEngine.query(jdbcValues);
+		
+		
+		return result;
 	}
 	
 	private void prettySql(Params params, Template personsTemp) {
@@ -51,7 +62,8 @@ public class TestJdbcQueries extends ATest {
 	}
 	
 	private void assertJohnDoe(Params params, Template template) throws SQLException {
-		ResultSet rs = getResultSet(params, template);
+		 JdbcQueryResult result = getResultSet(params, template);
+		ResultSet rs = result.getResultSet();
 		
 		assertNotNull(rs);
 		assertTrue(rs.next());
@@ -64,10 +76,12 @@ public class TestJdbcQueries extends ATest {
 		assertEquals("doe",rs.getString(7));
 		
 		assertFalse(rs.next());
+		result.close();
 	}
 	
 	private void assertLisa(Params params, Template template) throws SQLException {
-		ResultSet rs = getResultSet(params, template);
+		 JdbcQueryResult result = getResultSet(params, template);
+		 ResultSet rs = result.getResultSet();
 		
 		assertNotNull(rs);
 		assertTrue(rs.next());
@@ -80,6 +94,7 @@ public class TestJdbcQueries extends ATest {
 		assertEquals("Smith",rs.getString(7));
 		
 		assertFalse(rs.next());
+		result.close();
 	}
 	
 	public void testStringQuery() throws Exception {
@@ -135,11 +150,15 @@ public class TestJdbcQueries extends ATest {
 		
 		Template personsTemp = templates.getTemplate("persons");
 		prettySql(params, personsTemp);
-		ResultSet rs = getResultSet(params, personsTemp);
+		
+		JdbcQueryResult result = getResultSet(params, personsTemp);
+		ResultSet rs = result.getResultSet();
+		
 		assertTrue(rs.next());
 		assertTrue(rs.next());
 		assertTrue(rs.next());
 		assertFalse(rs.next());
+		result.close();
 	}
 	
 	public void testDateQuery() throws Exception {
@@ -164,12 +183,14 @@ public class TestJdbcQueries extends ATest {
 		Template personsTemp = templates.getTemplate("persons");
 		prettySql(params, personsTemp);
 		//should have two people alive
-		ResultSet rs = getResultSet(params, personsTemp);
+		JdbcQueryResult result = getResultSet(params, personsTemp);
+		ResultSet rs = result.getResultSet();
+		
 		assertTrue(rs.next());
 		assertTrue(rs.next());
 		assertFalse(rs.next());
 		
-		
+		result.close();
 	}
 	
 	public void testFloatQuery() throws Exception {
@@ -208,11 +229,15 @@ public class TestJdbcQueries extends ATest {
 		
 		Template personsTemp = templates.getTemplate("persons");
 		prettySql(params, personsTemp);
-		ResultSet rs = getResultSet(params, personsTemp);
+		JdbcQueryResult result = getResultSet(params, personsTemp);
+		ResultSet rs = result.getResultSet();
+		
 		//shold have two rows
 		assertTrue(rs.next());
 		assertTrue(rs.next());
 		assertFalse(rs.next());
+		
+		result.close();
 		
 	}
 	
@@ -227,11 +252,98 @@ public class TestJdbcQueries extends ATest {
 		
 		Template personsTemp = templates.getTemplate("persons");
 		prettySql(params, personsTemp);
-		ResultSet rs = getResultSet(params, personsTemp);
+		JdbcQueryResult result = getResultSet(params, personsTemp);
+		ResultSet rs = result.getResultSet();
+		
 		//shold have three rows
 		assertTrue(rs.next());
 		assertTrue(rs.next());
 		assertTrue(rs.next());
 		assertFalse(rs.next());
+		result.close();
+	}
+	
+	public void testExecute() throws Exception {
+		
+		JdbcEngineInput jdbcValues = new JdbcEngineInput();
+		jdbcValues.setAllowedOperators(BaseSqlOperators.OPERATORS);
+		Params params = new Params();
+		params.addParam("tableName", "foo_tablename");
+		
+		jdbcValues.setParams(params);
+		
+		//sql injection wouldn't be a issue when creating a db
+		Template template = executeUpdateTemplates.getTemplate("execute");
+		String result = TemplateParserEngine.parse(template, params);
+		
+		MockConnection mockConnection = new MockConnection(MockDatabase.getMemConnection());
+		Template createTableTemplate = new Template(result);
+		jdbcValues.setTemplate(createTableTemplate);
+		jdbcValues.setConnection(mockConnection);
+		assertFalse(JdbcTemplateParserEngine.execute(jdbcValues));
+		
+		List<PreparedStatement> stmts =  mockConnection.getCreatedPreparedStatement();
+		assertEquals(1, stmts.size());
+		PreparedStatement stmt = stmts.get(0);
+		assertTrue(stmt.isClosed());
+		
+	}
+	
+	public void testExecuteUpdate() throws Exception {
+		JdbcEngineInput jdbcValues = new JdbcEngineInput();
+		MockConnection mockConnection = new MockConnection(MockDatabase.getMemConnection());
+		Params params = new Params();
+		params.addParam("name", "testExecuteUpdateTable");
+		params.addParam("tid", 1);
+		jdbcValues.setConnection(mockConnection);
+		jdbcValues.setParams(params);
+		jdbcValues.setTemplate(executeUpdateTemplates.getTemplate("insert"));
+		//its false even though it suceeded
+		assertFalse(JdbcTemplateParserEngine.execute(jdbcValues));
+		List<PreparedStatement> statemetns = mockConnection.getCreatedPreparedStatement();
+		assertEquals(1, statemetns.size());
+		PreparedStatement stmt = statemetns.get(0);
+		assertTrue(stmt.isClosed());
+		mockConnection.reset();
+		
+		jdbcValues.setAllowedOperators(BaseSqlOperators.OPERATORS);
+		params = new Params();
+		params.addParam("name", "testExecuteUpdateTable");
+		params.addParam("newId", 1001);
+		
+		jdbcValues.setParams(params);
+		jdbcValues.setConnection(mockConnection);
+		Template template = executeUpdateTemplates.getTemplate("update_any");
+		
+		jdbcValues.setTemplate(template);
+		int countId = JdbcTemplateParserEngine.executeUpdate(jdbcValues);
+		assertEquals(1, countId);
+		
+		List<PreparedStatement> stmts =  mockConnection.getCreatedPreparedStatement();
+		assertEquals(1, stmts.size());
+		stmt = stmts.get(0);
+		assertTrue(stmt.isClosed());
+		mockConnection.commit();
+		mockConnection.reset();
+		
+		
+		params = new Params();
+		params.addParam("name", "testExecuteUpdateTable");
+		params.addParam("newId", 1091);
+		params.addParam("oldId", 1001);
+		
+		jdbcValues.setParams(params);
+		
+		template = executeUpdateTemplates.getTemplate("update");
+		
+		mockConnection.reset();
+		jdbcValues.setTemplate(template);
+		jdbcValues.setConnection(mockConnection);
+		assertEquals(1, JdbcTemplateParserEngine.executeUpdate(jdbcValues));
+		
+		stmts =  mockConnection.getCreatedPreparedStatement();
+		assertEquals(1, stmts.size());
+		stmt = stmts.get(0);
+		assertTrue(stmt.isClosed());
 	}
 }
